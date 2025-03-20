ccfd5b2d713e000fa6a792aa9256fb7670420684
acandoo <117209328+acandoo@users.noreply.github.com>
2025-03-20 17:12:00 -0400

    Add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,5 +12,7 @@ export default defineConfig({
 
   prefetch: {
     prefetchAll: true
-  }
+  },
+
+  integrations: [icon()]
 });
