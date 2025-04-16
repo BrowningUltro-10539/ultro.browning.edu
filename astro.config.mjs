a7cc5510a8e648476f3356893fc4b5b639179fa7
acandoo <117209328+acandoo@users.noreply.github.com>
2025-04-16 17:30:20 +0000

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ultro.browning.edu",
+  
   vite: {
     plugins: [tailwindcss()],
   },
